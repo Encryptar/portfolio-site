54132a63c8fc1e1a46ae9122bb5bff26612d57d3
Encryptar <64878818+Encryptar@users.noreply.github.com>
2022-03-07 12:33:30 +1300

    Changed Project Component
    Made it clickable for a link and added an image prop

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,10 +3,11 @@ import "./Project.scss"
 
 export default function Project(props) {
   return (
-    <div className='Project'>
+    <a href="https://github.com/Encryptar/IndustrialExpansion" className='Project'>
+        <img src={props.Image} alt="Composite Furnace"/>
         <h4>{props.Language}</h4>
         <h2>{props.Name}</h2>
         <p>{props.Desc}</p> 
-    </div>
+    </a>
   )
 }
--- src/components/Scroller/Scroller.jsx
+++ src/components/Scroller/Scroller.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Scroller.scss"
 import Project from '../Project/Project'
+import composite_furnace_front_on from "../../pictures/composite_furnace_front_on.png"
+
 
 export default function Projects() {
   return (
@@ -10,7 +12,9 @@ export default function Projects() {
           Name="Industrial Expansion" 
           Desc="A minecraft mod which adds tools, machines and other new features."
           Language="Java"
+          Image={composite_furnace_front_on}
         />
+        
       </div>
     </div>
   )
